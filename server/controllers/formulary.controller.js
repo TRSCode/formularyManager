ff9d6b2700f23debe568089589129408e3047d7a
Tony Schock <tony.schock@gmail.com>
2023-05-18 15:49:08 -0600

    CRUD added to controller with updated routes

--- server/controllers/formulary.controller.js
+++ server/controllers/formulary.controller.js
@@ -1,57 +1,42 @@
 const Formulary = require('../models/formulary.model');    /* this is new */
-module.exports.index = (request, response) => {
-    response.json({
-        message: "Hello World"
-    });
+
+module.exports = {
+    // CREATE MED
+    createMed: (req, res) => {
+        console.log(req.body)
+        Formulary.create(req.body)
+            .then(newMed => res.json(newMed))
+            .catch(err => res.status(400).json(err))
+    },
+
+    // READ ONE MED
+    getOneMed: (req,res) => {
+        console.log(req)
+        // Formulary.findById(req.params.id) // may need _id:req.params.id with findOne
+        Formulary.findOne({_id:req.params.id}) 
+            .then(oneMed => res.json(oneMed))
+            .catch(err => res.status(400).res.json(err))
+    },
+
+    // READ ALL MEDS
+    getAllMeds: (req,res) => {
+        Formulary.find().sort({medication:1}) //sort added see docs
+            .then(allMeds => res.json(allMeds))
+            .catch(err => res.status(400).json(err))
+    },
+
+    // UPDATE MED
+    updateMed: (req,res) => {
+        Formulary.findByIdAndUpdate({_id: req.params.id}, req.body, {new:true, runValidators:true})
+            .then(updateMed => res.json(updateMed))
+            .catch((err => res.status(400).json(err)))
+    },
+
+    // DELETE MED
+    deleteMed: (req,res) => {
+        Formulary.deleteOne({_id:req.params.id})
+        .then(deleteOneMed => res.json(deleteOneMed))
+        .catch((err) => console.log(err))
+    }
+
 }
-          /* The method below is new */
-module.exports.createMedication = (request, response) => {
-    // Mongoose's "create" method is run using our Person model to add a new person to our db's person collection.
-    // request.body will contain something like {firstName: "Billy", lastName: "Washington"} from the client
-    Formulary.create(request.body) //This will use whatever the body of the client's request sends over
-        .then(medication => response.json(medication))
-        .catch(err => response.json(err));
-}
-
-// const Product = require('../models/product.model');
-
-// module.exports = {
-//     // CREATE PRODUCT
-//     createProduct: (req, res) => {
-//         console.log(req.body)
-//         Product.create(req.body)
-//             .then(newProduct => res.json(newProduct))
-//             .catch(err => res.status(400).json(err))
-//     },
-
-//     // READ ONE PRODUCT
-//     getOneProduct: (req,res) => {
-//         console.log(req)
-//         // Product.findById(req.params.id) // may need _id:req.params.id with findOne
-//         Product.findOne({_id:req.params.id}) 
-//             .then(oneProduct => res.json(oneProduct))
-//             .catch(err => res.status(400).res.json(err))
-//     },
-
-//     // READ ALL PRODUCTS
-//     getAllProducts: (req,res) => {
-//         Product.find().sort({title:1}) //sort added see docs
-//             .then(allProducts => res.json(allProducts))
-//             .catch(err => res.status(400).json(err))
-//     },
-
-//     // UPDATE PRODUCT
-//     updateProduct: (req,res) => {
-//         Product.findByIdAndUpdate({_id: req.params.id}, req.body, {new:true, runValidators:true})
-//             .then(updateProduct => res.json(updateProduct))
-//             .catch((err => res.status(400).json(err)))
-//     },
-
-//     // DELETE PRODUCT
-//     deleteProduct: (req,res) => {
-//         Product.deleteOne({_id:req.params.id})
-//         .then(deleteOneProduct => res.json(deleteOneProduct))
-//         .catch((err) => console.log(err))
-//     }
-
-// }
--- server/routes/formulary.routes.js
+++ server/routes/formulary.routes.js
@@ -1,6 +1,9 @@
 const FormularyController = require('../controllers/formulary.controller');  
 module.exports = (app) => {
-    app.get('/api', FormularyController.index);
-    app.post('/api/formulary', FormularyController.createMedication);
 
+    app.post('/api/formulary', FormularyController.createMed);
+    app.get('/api/formulary', FormularyController.getAllMeds);
+    app.get('/api/formulary/:id', FormularyController.getOneMed);
+    app.patch('/api/formulary/:id', FormularyController.updateMed);
+    app.delete('/api/formulary/:id', FormularyController.deleteMed);
 }
