1523faa6ea392aac0ff5e14993efd0557a327315
Tony Schock <tony.schock@gmail.com>
2023-05-18 10:50:15 -0600

    updated formulary schema

--- server/models/formulary.model.js
+++ server/models/formulary.model.js
@@ -3,18 +3,26 @@ const FormularySchema = new mongoose.Schema({
     medication: { type: String },
     description: { type: String },
     // this includes strength and dosage form
-    units: { type: String },
-    // add enum for EA, PG, BX, TU etc
-    authorizedAmount: { type: String },
-    onHand: { type: String },
+    units: { type: String,
+            enum: ['EA', 'PG', 'VI', 'TU', 'BT', 'BX', 'KT', 'CO', 'other']
+    },
+    authorizedAmount: { type: Number },
+    onHand: { type: Number },
     lotNumber: { type: String },
-    expiration: { date: Number },
+    expiration: { type: Date },
     nsn: { type: String },
     ndc: { type: String },
     supplier: { type: String },
-    ciic: { type: String },
-    dispenseLevel: { type: String },
-    storageLocation: { type: String }
-    // add enum for possible locations
+    ciic: { type: String,
+            enum: ['Q', 'R', 'U', 'J']
+    },
+    dispenseLevel: { type: String,
+                    enum: ['Presciber', 'Delegate']
+    },
+    storageLocation: { 
+        type: String,
+        enum: ['Shelf 1', 'Shelf 2', 'Shelf 3', 'Airway Kit', 'Safe']
+    },
+    notes: {type: String}
 }, { timestamps: true });
 module.exports = mongoose.model('Formulary', FormularySchema);
