cfb38c177c505119777f8f2fa8489cbc0a1101a8
Tony Schock <tony.schock@gmail.com>
2023-05-22 23:55:18 -0600

    added logo

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,8 +1,9 @@
 // Navbar created using tutorial found on https://www.youtube.com/watch?v=SLfhMt5OUPI
+import logoMM from './logoMM.png'
 export default function Navbar() {
     
     return <nav className="nav">
-        <a href="/dashboard" className="site-title">Med Manager</a>
+        <a href="/dashboard" className="site-title"><img src={logoMM} className="logoSize" alt="Med Manager" />Med Manager</a>
         <ul>
             <CustomLink href="/formulary">Formulary</CustomLink>
             <CustomLink href="/add">Add Med</CustomLink>
